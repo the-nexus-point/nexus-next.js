3737c38a93e087723b01d3844434d39144397ed0
Harsh <harshkeshari100@gmail.com>
2023-06-02 19:16:54 +0530

    added color theme of app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Nexus',
-}
+  title: "Nexus",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" style={{ height: "100%" }}>
+      <body className={`${inter.className} text-white`}>
+        <div>
+          <div>NAVBAR</div>
+          {children}
+          <div>FOOTER</div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }

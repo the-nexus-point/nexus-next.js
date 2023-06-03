2f3da1fa6520c86d4cdabcc4e2599d3fe47b6c5c
Harsh <harshkeshari100@gmail.com>
2023-06-03 22:54:18 +0530

    improved auth UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,28 +4,37 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <form className="bg-black drop-shadow-2xl rounded px-8 pt-10 pb-10 mb-4">
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-          />
+        <h3 className="text-center mb-5">Log In with College Id</h3>
+        <div className="mb-5">
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 bg-neutral-900 text-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 bg-neutral-900 text-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
         </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Login
+        <hr />
+        <div className="flex items-center justify-center mt-5 ">
+          <button className=" border border-gray-700 hover:border-gray-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Sign in with Google
           </button>
         </div>
       </form>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,61 +3,72 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="bg-black drop-shadow-2xl rounded px-8 pt-10 pb-10 mb-4">
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="fullName"
-            type="text"
-            placeholder="Full Name"
-          />
+      <form className="bg-black drop-shadow-2xl rounded px-8 pt-10 pb-10 mb-4 w-1/4">
+        <h3 className="text-center mb-5">Sign Up with College Id</h3>
+        <div className="mb-5">
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+              id="fullName"
+              type="text"
+              placeholder="Full Name"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+              id="libraryId"
+              type="text"
+              placeholder="Library ID"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex space-x-5 justify-between">
+            <div className="mb-4 w-1/2">
+              <input
+                className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+                id="department"
+                type="text"
+                placeholder="Department"
+              />
+            </div>
+            <div className="mb-4 w-1/2">
+              <input
+                className="shadow appearance-none border border-gray-700 hover:border-gray-400 rounded w-full py-2 px-3 text-white leading-tight bg-neutral-900 focus:outline-none focus:shadow-outline"
+                id="year"
+                type="text"
+                placeholder="Year"
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="libraryId"
-            type="text"
-            placeholder="Library ID"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="department"
-            type="text"
-            placeholder="Department"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="year"
-            type="text"
-            placeholder="Year"
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign Up
+        <hr />
+        <div className="flex items-center justify-center mt-5 ">
+          <button className="border border-gray-700 hover:border-gray-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Sign in with Google
           </button>
         </div>
       </form>

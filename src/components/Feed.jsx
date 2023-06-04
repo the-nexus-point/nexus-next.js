910d9c5d7e6ef478903e51d60712a662eaa7fd25
Harsh <harshkeshari100@gmail.com>
2023-06-04 15:08:21 +0530

    touching feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -33,51 +33,51 @@ const Feed = () => {
             </button>
           </div>
           <div className="flex-shrink-0 mt-auto mb-5 mx-5">
-            <button className="border rounded py-2 px-4 w-full">Feedback</button>
+            <button className="border rounded py-2 px-4 w-full">
+              Feedback
+            </button>
           </div>
         </div>
-        <div className="w-3/4 h-full flex flex-col">
-          <div className="overflow-y-auto">
-            <div className="p-20">
-              <h1 className="text-4xl">Post 1</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus iste tempora minima. Voluptatum tempore eum
-                deserunt eligendi. Exercitationem officiis voluptates hic nemo
-                consectetur deserunt placeat ad minima! Culpa, consequatur
-                molestias.
-              </p>
-            </div>
-            <div className="p-20">
-              <h1 className="text-4xl">Post 2</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus iste tempora minima. Voluptatum tempore eum
-                deserunt eligendi. Exercitationem officiis voluptates hic nemo
-                consectetur deserunt placeat ad minima! Culpa, consequatur
-                molestias.
-              </p>
-            </div>
-            <div className="p-20">
-              <h1 className="text-4xl">Post 3</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus iste tempora minima. Voluptatum tempore eum
-                deserunt eligendi. Exercitationem officiis voluptates hic nemo
-                consectetur deserunt placeat ad minima! Culpa, consequatur
-                molestias.
-              </p>
-            </div>
-            <div className="p-20">
-              <h1 className="text-4xl">Post 4</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus iste tempora minima. Voluptatum tempore eum
-                deserunt eligendi. Exercitationem officiis voluptates hic nemo
-                consectetur deserunt placeat ad minima! Culpa, consequatur
-                molestias.
-              </p>
-            </div>
+        <div className="w-3/4 h-full flex flex-col overflow-y-auto scrollbar-none">
+          <div className="p-20">
+            <h1 className="text-4xl">Post 1</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus iste tempora minima. Voluptatum tempore eum
+              deserunt eligendi. Exercitationem officiis voluptates hic nemo
+              consectetur deserunt placeat ad minima! Culpa, consequatur
+              molestias.
+            </p>
+          </div>
+          <div className="p-20">
+            <h1 className="text-4xl">Post 2</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus iste tempora minima. Voluptatum tempore eum
+              deserunt eligendi. Exercitationem officiis voluptates hic nemo
+              consectetur deserunt placeat ad minima! Culpa, consequatur
+              molestias.
+            </p>
+          </div>
+          <div className="p-20">
+            <h1 className="text-4xl">Post 3</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus iste tempora minima. Voluptatum tempore eum
+              deserunt eligendi. Exercitationem officiis voluptates hic nemo
+              consectetur deserunt placeat ad minima! Culpa, consequatur
+              molestias.
+            </p>
+          </div>
+          <div className="p-20">
+            <h1 className="text-4xl">Post 4</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus iste tempora minima. Voluptatum tempore eum
+              deserunt eligendi. Exercitationem officiis voluptates hic nemo
+              consectetur deserunt placeat ad minima! Culpa, consequatur
+              molestias.
+            </p>
           </div>
         </div>
       </div>

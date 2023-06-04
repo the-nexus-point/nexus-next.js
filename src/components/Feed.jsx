40f3671db869c9a2d1ae6ef52d635b86eaf713da
Harsh <harshkeshari100@gmail.com>
2023-06-04 13:12:23 +0530

    touching feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,3 +1,6 @@
+import { AiFillHome, AiFillFlag } from "react-icons/ai";
+import { SiMarketo } from "react-icons/si";
+
 const Feed = () => {
   return (
     <>
@@ -10,15 +13,22 @@ const Feed = () => {
         </div>
       </div>
       <div className="h-full w-full flex">
-        <div className="h-full w-1/4 flex flex-col justify-between pb-10 border-r border-gray-700">
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
-          <h2>Routes</h2>
+        <div className="h-full w-1/4 flex flex-col space-y-5 justify-between pb-10 border-r border-gray-700 my-5">
+          <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+            <AiFillHome className="w-8 h-8" />
+            Feed
+          </button>
+          <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+            <AiFillFlag className="w-8 h-8" />
+            Challenges
+          </button>
+          <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+            <SiMarketo className="w-8 h-8" />
+            Market Place
+          </button>
+          <button className="rounded-full bg-purple-500 hover:bg-purple-700 font-bold py-2 px-4 focus:outline-none focus:shadow-outline mx-5">
+            Create Post
+          </button>
         </div>
         <div className="w-3/4 h-1/10">
           <h1>Main content</h1>

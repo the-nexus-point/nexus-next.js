eab63efa90dbdaf0c50e3a56873657867b15518a
Harsh <harshkeshari100@gmail.com>
2023-06-05 23:10:53 +0530

    breaking the container of user profile

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,6 @@
-import { AiOutlineUser, AiFillMessage, AiOutlineLogout } from "react-icons/ai";
-import { RxActivityLog } from "react-icons/rx";
-import { BiEditAlt } from "react-icons/bi";
-import { CiSettings } from "react-icons/ci";
+import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import UserContainer from "./shared/UserContainer";
 
 const Profile = () => {
   const user = {
@@ -12,53 +10,16 @@ const Profile = () => {
     bio: "Software Developer | Tech Enthusiast",
     location: "New York, USA",
     website: "https://www.example.com",
+    upvotes: 100,
+    department: "CSAI",
+    challengeRanking: 5,
+    tags: ["JavaScript", "React", "Node.js"],
   };
 
   return (
-    <div className="w-full h-screen flex flex-col">
-      <div className="h-full w-full flex overflow-hidden">
-        <div className="h-full w-1/4 flex flex-col justify-between border-r border-gray-700">
-          <div className="flex flex-col space-y-5 justify-between my-5">
-            <div className="h-14 w-14 mx-8 cursor-pointer">
-              <img src="/img/nexus-website-favicon-white.png" alt="Logo" />
-            </div>
-            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
-              <AiOutlineUser className="w-8 h-8 mx-2" />
-              Profile
-            </button>
-            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
-              <RxActivityLog className="w-8 h-8 mx-2" />
-              Activities
-            </button>
-            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
-              <BiEditAlt className="w-8 h-8 mx-2" />
-              Edit Profile
-            </button>
-            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
-              <CiSettings className="w-8 h-8 mx-2" />
-              Account Settings
-            </button>
-          </div>
-          <div className="flex justify-between mt-auto mb-5 mx-5">
-            <button className="flex items-center border border-gray-700 hover:border-gray-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              <AiFillMessage className="mx-2" />
-              <span>feedback</span>
-            </button>
-            <button className="logout-button flex items-center space-x-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-              <AiOutlineLogout className="text-lg" />
-              <span>Logout</span>
-            </button>
-          </div>
-        </div>
-        <div className="w-3/4 h-full overflow-y-auto scrollbar-none">
-          <div className="flex justify-between border-b border-gray-700 p-2">
-            <button className="text-3xl mx-8 hover:text-black hover:opacity-75">
-              <FaUserCircle className="" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <UserContainer>
+      
+    </UserContainer>
   );
 };
 
---
+++ src/components/shared/UserContainer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { AiOutlineUser, AiFillMessage, AiOutlineLogout } from "react-icons/ai";
+import { RxActivityLog } from "react-icons/rx";
+import { BiEditAlt } from "react-icons/bi";
+import { CiSettings } from "react-icons/ci";
+
+const UserContainer = ({ children }) => {
+  return (
+    <div className="w-full h-screen flex flex-col">
+      <div className="h-full w-full flex overflow-hidden">
+        <div className="h-full w-1/4 flex flex-col justify-between border-r border-gray-700">
+          <div className="flex flex-col space-y-5 justify-between my-5">
+            <div className="h-14 w-14 mx-8 cursor-pointer">
+              <img src="/img/nexus-website-favicon-white.png" alt="Logo" />
+            </div>
+            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+              <AiOutlineUser className="w-8 h-8 mx-2" />
+              Profile
+            </button>
+            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+              <RxActivityLog className="w-8 h-8 mx-2" />
+              Activities
+            </button>
+            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+              <BiEditAlt className="w-8 h-8 mx-2" />
+              Edit Profile
+            </button>
+            <button className="flex items-center rounded bg-neutral-900/25 hover:bg-neutral-900/40 font-bold py-2 px-4 mx-5">
+              <CiSettings className="w-8 h-8 mx-2" />
+              Account Settings
+            </button>
+          </div>
+          <div className="flex justify-between mt-auto mb-5 mx-5">
+            <button className="flex items-center border border-gray-700 hover:border-gray-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              <AiFillMessage className="mx-2" />
+              <span>Feedback</span>
+            </button>
+            <button className="logout-button flex items-center space-x-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              <AiOutlineLogout className="text-lg" />
+              <span>Logout</span>
+            </button>
+          </div>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+export default UserContainer;

abd61617edc8d37b8f0d8398c897a94691bf60a1
Harsh <harshkeshari100@gmail.com>
2023-06-05 19:08:57 +0530

    added marketplace route

---
+++ src/app/marketplace/page.jsx
@@ -0,0 +1,10 @@
+import Marketplace from "../../components/Marketplace";
+const ChallengesPage = () => {
+  return (
+    <div>
+      <Marketplace />
+    </div>
+  );
+};
+
+export default ChallengesPage;
---
+++ src/components/Marketplace.jsx
@@ -0,0 +1,16 @@
+import Container from "./shared/Container";
+
+const Feed = () => {
+  return (
+    <Container activeRoute="marketplace">
+      <div className="p-2 flex flex-col space-y-1">
+        <h1>Hello</h1>
+        <h1>Hello</h1>
+        <h1>Hello</h1>
+        <h1>Hello</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Feed;

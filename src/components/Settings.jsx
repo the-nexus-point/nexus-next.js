75cd881e058a54a233c30df6431155313bfdcf6d
Harsh <harshkeshari100@gmail.com>
2023-06-06 19:25:04 +0530

    error message for unfinished route

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import UserContainer from "./shared/UserContainer";
+import { BsFillEmojiDizzyFill } from "react-icons/bs";
 
 const Settings = () => {
   return (
     <UserContainer>
-      <div>
+      <div className="flex items-center justify-center p-5 space-x-5">
+        <BsFillEmojiDizzyFill className="w-10 h-10" />
         <h1 className="text-4xl font-bold">
           This feature will be available soon...
         </h1>

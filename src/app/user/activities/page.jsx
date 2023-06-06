829ae54a1c2adc3240aaea93da9b653d6f4586a2
Harsh <harshkeshari100@gmail.com>
2023-06-06 19:03:08 +0530

    init the activity route

---
+++ src/app/user/activities/page.jsx
@@ -0,0 +1,11 @@
+import Activities from "@/components/Activities";
+
+const ActivitiesPage = () => {
+  return (
+    <div>
+      <Activities />
+    </div>
+  );
+};
+
+export default ActivitiesPage;
---
+++ src/components/Activities.jsx
@@ -0,0 +1,11 @@
+import UserContainer from "./shared/UserContainer";
+
+const Activities = () => {
+  return (
+    <UserContainer>
+      <h1>Hello</h1>
+    </UserContainer>
+  );
+};
+
+export default Activities;

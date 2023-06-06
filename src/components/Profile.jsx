e11e2af9f928e8dd8a23e9321d27399a5c8a939e
Harsh <harshkeshari100@gmail.com>
2023-06-06 18:52:44 +0530

    improving user ui

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -59,7 +59,7 @@ const Profile = () => {
             <div className="flex items-center justify-center space-x-1">
               <AiFillFlag className="w-5 h-5" />
               <p className="text-sm">
-                Challenge Ranking: {user.challengeRanking}
+                Challenge Score: {user.challengeRanking}
               </p>
             </div>
             <div className="flex space-x-3 mt-4">

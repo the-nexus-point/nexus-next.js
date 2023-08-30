b933d90b5e0e09c6db3b5d4321eb6dbdbcccde30
Harsh Keshari <harshkeshari@Harshs-MacBook-Air.local>
2023-08-30 15:22:37 +0530

    changing UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
 import Feed from "@/components/Feed";
 import Home from "@/components/Home";
 
-const loggedIn = true;
 
 export default () => {
-  return loggedIn ? <Feed /> : <Home />;
+  return  <Home />;
 };
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Post from "./shared/Post";
 
 const Feed = () => {

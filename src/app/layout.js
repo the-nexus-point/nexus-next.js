4e2bb449ca3d75cf678c96e751c92433da829f4c
hrshkshri <harshkeshari100@gmail.com>
2023-09-23 16:04:59 +0530

    signin fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,31 @@
+"use client";
 import Layout from "@/components/layout/Layout";
 import "./globals.css";
 import { ChakraProviders } from "../services/Chakraproviders";
 import { NextAuthProvider } from "@/services/NextauthProvider";
+import { usePathname } from "next/navigation";
 
-export const metadata = {
-  title: "Nexus",
-};
+// export const metadata = {
+//   title: "Nexus",
+// }; 
+
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const isSignInPage = pathname.startsWith("/auth/signin");
+
+  if (isSignInPage) {
+    return (
+      <html lang="en">
+        <body className={`text-white`}>
+          <NextAuthProvider>
+            <ChakraProviders>{children}</ChakraProviders>
+          </NextAuthProvider>
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <body className={`text-white`}>

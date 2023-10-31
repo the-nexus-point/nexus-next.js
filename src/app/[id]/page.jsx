cf35af25d1bf21baf8e7f8508d129f2a522d292a
hrshkshri <harshkeshari100@gmail.com>
2023-11-01 04:32:50 +0530

    jwt verify error checkpoint

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,7 +1,12 @@
+"use client"
 import Profile from "@/components/account/Profile";
-import React from "react";
+import { isUserAuthenticated } from "@/services/auth";
+
 
 const User = () => {
+  if (!isUserAuthenticated()) {
+    window.location.href = "/signup";
+  }
   return (
     <div>
       <Profile />
--- src/components/account/Profile.jsx
+++ src/components/account/Profile.jsx
@@ -1,9 +1,32 @@
 "use client";
-import { Box, Flex, Image, Tag, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, Tag } from '@chakra-ui/react';
 import React from 'react';
 import { AiFillTrophy } from 'react-icons/ai';
+import { useState, useEffect } from 'react';
+import jwt from 'jsonwebtoken';
 
 function Profile() {
+
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token'); // Get the token from local storage
+        // console.log(token);
+        const secret = 'thisisasecret';
+        try {
+            const decodedToken = jwt.verify(token, 'thisisasecret');
+            if (!decodedToken || !decodedToken.userId) {
+                // Handle the case where the user's ID is not available in the token
+                return;
+            }
+            console.log(decodedToken);
+        } catch (error) {
+            console.error('Error decoding JWT:', error);
+        }
+    }, []);
+
+
+
     return (
         <>
             <div>
@@ -21,12 +44,19 @@ function Profile() {
                             </div>
                         </div>
                     </Box>
-                    <Box flex='2'>
-                        <div className='flex space-y-3 flex-col justify-center items-start'>
-                            <Tag>bio of the user bio of the user bio of the user bio of the user. bio of the user bio of the user bio of the user bio of the user. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Tag>
-                            <Tag> username </Tag>
-                            <Tag> email </Tag>
-                            <Tag> Library Id </Tag>
+                    <Box flex='2' className='flex space-y-3 flex-col justify-center items-start'>
+                        <div className='flex justify-between px-3 py-4'>
+                            <div className='flex flex-col justify-between space-y-3 mx-1'>
+                                <Tag> username </Tag>
+                                <Tag> email </Tag>
+                                <Tag> libId </Tag>
+                                <Tag> Branch </Tag>
+                            </div>
+                            <div className='w-full flex mx-1'>
+                                <Tag>bio of the user </Tag>
+                            </div>
+                        </div>
+                        <div className='w-full flex justify-between items-center px-3 py-4'>
                             <Tag> hackerrankId </Tag>
                             <Tag> codechefId </Tag>
                             <Tag> codeforcesId </Tag>

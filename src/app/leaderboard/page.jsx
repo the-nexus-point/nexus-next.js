e4bca230fe826fedbac8d04a3978970056f86f06
hrshkshri <harshkeshari100@gmail.com>
2023-11-04 22:21:56 +0530

    fixed localStorage issue

--- src/app/leaderboard/page.jsx
+++ src/app/leaderboard/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Leaderboard from '@/components/leaderboard/Leaderboard'
 import { isUserAuthenticated } from '@/services/auth'
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import SignUp from "@/components/authentication/SignUp";
 import React from 'react'
 import { isUserAuthenticated } from '@/services/auth'
